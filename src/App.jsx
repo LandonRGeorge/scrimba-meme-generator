1368afad7db02aca2969b4caecf8931d2df9ccce
LandonRGeorge <landon.robert.george@gmail.com>
2024-04-06 17:22:39 -0500

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
-function Nav() {
+function Header() {
   return (
-    <nav className="nav">
+    <header className="header">
       <h1>Meme Generator</h1>
       <p>React Course - Project 3</p>
-    </nav>
+    </header>
   )
 }
 
 function Main() {
+  const [templates, setTemplates] = useState([]);
+  useEffect(() => {
+    console.log("getting data...")
+    fetch("https://api.memegen.link/templates")
+      .then((res) => res.json())
+      .then((data) => setTemplates(data))
+  }, [])
+
+  const [templateIndex, setTemplateIndex] = useState(null)
   const [topText, setTopText] = useState('Shut up')
   const [bottomText, setBottomText] = useState('And take my money')
+
+  function formHandler(e) {
+    e.preventDefault()
+    const randomIndex = Math.floor(Math.random() * templates.length)
+    setTemplateIndex(randomIndex)
+    const t = templates[randomIndex]
+    setTopText(t.example.text[0])
+    setBottomText(t.example.text[1])
+  }
+
+  let imgURL = "/images/default.png"
+
+  if (templateIndex) {
+    const template = templates[templateIndex];
+    imgURL = template.blank
+  }
+
   return (
     <div className="inner-container">
-      <form className="form">
+      <form className="form" onSubmit={formHandler}>
         <div className="form-controls">
           <div className="form-control">
             <label htmlFor="form-top-text">Top text</label>
@@ -28,7 +54,7 @@ function Main() {
         <button className="form-submit">Get a new meme image</button>
       </form>
       <div className="image">
-        <img src="/images/default.png" alt=""/>
+        <img src={imgURL} alt=""/>
         <p className="image-text image-top-text">{topText}</p>
         <p className="image-text image-bottom-text">{bottomText}</p>
       </div>
@@ -40,7 +66,7 @@ function Main() {
 function App() {
   return (
     <div className="main-container">
-      <Nav/>
+      <Header/>
       <Main/>
     </div>
   )
